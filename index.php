322d0c93994300df389846e9402a7c4fa62cc04a
Tasikul Islam <iamtasikul@gmail.com>
2020-04-13 12:33:46 +0530

    Added Screenshot and Blog Info

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php bloginfo( 'name' )?>
+<!-- direct echo -->
+
+
+<!-- <?php $title=get_bloginfo('name');
+echo strtoupper($title);
+?> -->
+
+<!-- 
+    https://developer.wordpress.org/reference/functions/get_bloginfo/
+ -->
+
