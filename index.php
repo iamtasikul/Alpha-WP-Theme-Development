73a40ac9b2c6dce4f6ebbb5d19f08081961f5b29
Tasikul Islam <iamtasikul@gmail.com>
2020-04-15 14:43:58 +0530

    get prefix

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
-<?php bloginfo( 'name' )?>
+<?php bloginfo('name')?>
 <!-- direct echo -->
 
 
-<!-- <?php $title=get_bloginfo('name');
+<!-- <?php $title = get_bloginfo('name');
 echo strtoupper($title);
 ?> -->
 
-<!-- 
+<!--
     https://developer.wordpress.org/reference/functions/get_bloginfo/
  -->
 
