f2aca064a9d00ed8138b012cbb044a0acb432da2
Tasikul Islam <iamtasikul@gmail.com>
2020-04-16 23:22:33 +0530

    3.13 index page divided by Multiple Parts

--- single.php
+++ single.php
@@ -1,27 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <?php wp_head(); ?>
-
-</head>
+<?php get_header(); ?>
 
 <body <?php body_class(); ?>>
-    <div class="header">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h3 class="tagline">
-                        <?php bloginfo("description"); ?>
-                    </h3>
-                    <h1 class="align-self-center display-1 text-center heading">
-                        <?php bloginfo("name"); ?>
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php get_template_part("hero"); ?>
     <div class="posts">
         <?php
         while (have_posts()) {
@@ -76,16 +56,4 @@
         </div>
     </div>
 </div>
-<div class="footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                &copy; LWHH - All Rights Reserved
-            </div>
-        </div>
-    </div>
-</div>
-<?php wp_footer(); ?>
-</body>
-
-</html>
+<?php get_footer(); ?>
