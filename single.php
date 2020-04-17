52106db478e3fbfbcb309a7ba53678cfe53662d4
Tasikul Islam <iamtasikul@gmail.com>
2020-04-17 11:21:35 +0530

    3.17 widget area (sidebar) registration and show to single post

--- single.php
+++ single.php
@@ -2,61 +2,76 @@
 
 <body <?php body_class(); ?>>
     <?php get_template_part("hero"); ?>
-    <div class="posts">
-        <?php
-        while (have_posts()) {
-            the_post();
-        ?>
-            <div class="post" <?php post_class() ?>>
-                <div class="container ">
-                    <div class="row">
-                        <div class="col-md-10 offset-md-1">
-                            <h2 class="post-title text-center"><?php the_title(); ?></h2>
-                            <p class="text-center">
-                                <strong><?php the_author(); ?></strong><br />
-                                <?php echo get_the_date(); ?>
-                                <?php get_the_tag_list("<ul class='list-unstyled'>", "<li></li>", "<li></ul>"); ?>
-                            </p>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class=" col-md-10 offset-md-1">
-                            <p>
-                                <?php if (has_post_thumbnail()) {
-                                    the_post_thumbnail("large", "class='img-fluid'");
-                                } ?>
-                                <?php
-                                the_content();
-                                next_post_link();
-                                echo "<br/>";
-                                previous_post_link();
-                                ?>
-                            </p>
-                        </div>
-                        <?php if (comments_open()) : ?>
-                            <div class="col-md-10 offset-md-1 ">
-                                <?php comments_template(); ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="posts">
+                    <?php
+                    while (have_posts()) {
+                        the_post();
+                    ?>
+                        <div class="post" <?php post_class() ?>>
+                            <div class="container ">
+                                <div class="row">
+                                    <div class="col-md-10 offset-md-1">
+                                        <h2 class="post-title text-center"><?php the_title(); ?></h2>
+                                        <p class="text-center">
+                                            <strong><?php the_author(); ?></strong><br />
+                                            <?php echo get_the_date(); ?>
+                                            <?php get_the_tag_list("<ul class='list-unstyled'>", "<li></li>", "<li></ul>"); ?>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class=" col-md-10 offset-md-1">
+                                        <p>
+                                            <?php if (has_post_thumbnail()) {
+                                                the_post_thumbnail("large", "class='img-fluid'");
+                                            } ?>
+                                            <?php
+                                            the_content();
+                                            next_post_link();
+                                            echo "<br/>";
+                                            previous_post_link();
+                                            ?>
+                                        </p>
+                                    </div>
+                                    <?php if (comments_open()) : ?>
+                                        <div class="col-md-10 offset-md-1 ">
+                                            <?php comments_template(); ?>
+                                        </div>
+                                    <?php endif; ?>
+                                </div>
                             </div>
-                        <?php endif; ?>
+                        </div>
+
+                </div>
+
+            <?php
+                    }
+            ?>
+            <div class="container post-pagination">
+                <div class="row">
+                    <div class="col-md-4"></div>
+                    <div class="col-md-8">
+                        <?php the_posts_pagination(array(
+                            "screen_reader_text" => " ",
+                            'prev_text' => "New Posts",
+                            'next_text' => "Old Posts"
+                        )); ?>
                     </div>
                 </div>
             </div>
+            </div>
+            <div class="col-md-4">
 
-    </div>
-
-<?php
-        }
-?>
-<div class="container post-pagination">
-    <div class="row">
-        <div class="col-md-4"></div>
-        <div class="col-md-8">
-            <?php the_posts_pagination(array(
-                "screen_reader_text" => " ",
-                'prev_text' => "New Posts",
-                'next_text' => "Old Posts"
-            )); ?>
+                <?php
+                if (is_active_sidebar('sidebar-1')) {
+                    dynamic_sidebar('sidebar-1');
+                }
+                ?>
+            </div>
         </div>
     </div>
-</div>
-<?php get_footer(); ?>
+
+    <?php get_footer(); ?>
