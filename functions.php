f8ed1b16d72bc41db47f98cabf1f3d349a8dcb4d
Tasikul Islam <iamtasikul@gmail.com>
2020-04-20 17:14:22 +0530

    3.24 Single Page View and Page Template

--- functions.php
+++ functions.php
@@ -22,8 +22,8 @@ function alpha_assets()
     wp_enqueue_style("alpha", get_stylesheet_uri(), null, VERSION);
     wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
     wp_enqueue_style("featherlight-css", "//cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.css");
-    wp_enqueue_script("featherlight-js", "//cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.js", array('jquery'), "0.0.1", true);
-    wp_enqueue_script("alpha-main", get_template_directory_uri() . "/assets/js/main.js", array("jquery", "featherlight-js"), VERSION, true);
+    wp_enqueue_script("featherlight-js", "//cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.js", array('jquery'), null, true);
+    wp_enqueue_script("alpha-main",  get_theme_file_uri() . "/assets/js/main.js", array("jquery", "featherlight-js"), VERSION, true);
 }
 add_action("wp_enqueue_scripts", "alpha_assets");
 
