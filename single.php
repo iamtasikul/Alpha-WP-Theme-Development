9c5f97f47b5e31e80e07cc567cb9355e5eaf372a
Tasikul Islam <iamtasikul@gmail.com>
2020-04-24 09:50:42 +0530

    pagination for single post

--- single.php
+++ single.php
@@ -34,9 +34,10 @@
                                             } ?>
                                             <?php
                                             the_content();
-                                            next_post_link();
-                                            echo "<br/>";
-                                            previous_post_link();
+                                            wp_link_pages();
+                                            // next_post_link();
+                                            // echo "<br/>";
+                                            // previous_post_link();
                                             ?>
                                         </p>
                                     </div>
