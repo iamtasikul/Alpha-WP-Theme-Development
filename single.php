8b7e5e035a0058f9cab6c185365f9624335dbe86
Tasikul Islam <iamtasikul@gmail.com>
2020-04-27 22:37:03 +0530

    Show Comment list

--- single.php
+++ single.php
@@ -79,6 +79,13 @@ if (!is_active_sidebar("sidebar-1")) {
                                                 </div>
                                             </div>
                                         </div>
+                                        <?php if (!post_password_required()) : ?>
+                                            <div class="col-md-12">
+                                                <?php
+                                                comments_template();
+                                                ?>
+                                            </div>
+                                        <?php endif; ?>
                                     </div>
                                 </div>
                             </div>
