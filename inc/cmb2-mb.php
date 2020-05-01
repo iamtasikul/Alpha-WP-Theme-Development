92e27bd4af6c20eb235fdce96348576ddaf02dfc
Tasikul Islam <iamtasikul@gmail.com>
2020-05-01 12:07:18 +0530

    17.6 Pricing Table Using  CMB2 Repeatable Field

--- inc/cmb2-mb.php
+++ inc/cmb2-mb.php
@@ -67,3 +67,43 @@ function cmb2_add_image_info_metabox()
         )
     ));
 }
+function cmb2_add_pricingtable()
+{
+
+    $prefix = '_alpha_pt_';
+
+    $cmb = new_cmb2_box(array(
+        'id'           => $prefix . 'pricing_table_mb',
+        'title'        => __('Pricing Table', 'alpha'),
+        'object_types' => array('page'),
+        'context'      => 'normal',
+        'priority'     => 'default',
+    ));
+
+    $group = $cmb->add_field(array(
+        'name' => __('Pricing Table', 'alpha'),
+        'id'   => $prefix . 'pricing_table',
+        'type' => 'group',
+    ));
+
+    $cmb->add_group_field($group, array(
+        'name'       => __('Caption', 'alpha'),
+        'id'         => $prefix . 'pricing_caption',
+        'type'       => 'text',
+    ));
+
+    $cmb->add_group_field($group, array(
+        'name'       => __('Pricing Option', 'alpha'),
+        'id'         => $prefix . 'pricing_option',
+        'type'       => 'text',
+        'repeatable' => true
+    ));
+
+    $cmb->add_group_field($group, array(
+        'name'       => __('Price', 'alpha'),
+        'id'         => $prefix . 'price',
+        'type'       => 'text',
+    ));
+}
+
+add_action('cmb2_init', 'cmb2_add_pricingtable');
